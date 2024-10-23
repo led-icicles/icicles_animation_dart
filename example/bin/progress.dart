a5455420f293d6f6d5c788c29d95aa9bdadd867f
gonuit <kamilklyta341@gmail.com>
2024-10-23 23:00:43 +0200

    feat: improve example

--- example/bin/progress.dart
+++ example/bin/progress.dart
@@ -3,11 +3,11 @@ import 'package:icicles_animation_dart/icicles_animation_dart.dart';
 void main() async {
   final animation = Animation(
     'Progress',
-    xCount: 32,
+    xCount: 64,
     yCount: 8,
     radioPanelsCount: 2,
     radioPanelPixelCount: 60,
-    loopsCount: 10,
+    loopsCount: 2,
     framerate: Framerate.fps60,
     framerateBehavior: FramerateBehavior.drop,
     useRgb565: false,
@@ -15,33 +15,40 @@ void main() async {
   );
 
   final icicles = Icicles(animation);
-  // final tween = Tween.curve(
-  //   curve: Curves.bounceOut,
-  //   parent: Tween.number(
-  //     begin: 0.0,
-  //     end: 1.0,
-  //   ),
-  // );
+  final tween = Tween.curve(
+    curve: Curves.linear,
+    parent: Tween.number(
+      begin: 0.0,
+      end: 1.0,
+    ),
+  );
 
   void bounce(List<Color> colors) {
     Color backgroundColor = Colors.black;
     for (final foregroundColor in colors) {
-      for (double i = 0; i < 1; i += 0.015) {
+      for (double i = 0; i < 1; i += 0.010) {
         icicles
           ..setAllPixelsColor(backgroundColor)
           ..setAllRadioPanelsColor(backgroundColor);
-        final progress = i;
-        final turnedOnCount = (progress * animation.header.xCount).round();
+        final progress = tween.transform(i);
+        final turnedOnCount =
+            (progress * (animation.header.xCount / 2)).round();
         for (int x = 0; x < turnedOnCount; x++) {
-          icicles.setColumnColor(x, foregroundColor);
+          icicles
+            ..setColumnColor(x, foregroundColor)
+            ..setColumnColor(
+                (animation.header.xCount - 1 - x), foregroundColor);
         }
 
         final radioTurnedOnCount =
             (progress * animation.header.radioPanelPixelCount).round();
 
-        for (final panel in animation.currentView.radioPanels) {
+        for (final (panelIndex, panel)
+            in animation.currentView.radioPanels.indexed) {
+          final fromLeft = panelIndex % 2 == 0;
           for (int i = 0; i < radioTurnedOnCount; i++) {
-            icicles.setRadioPanelPixelColor(panel.index, i, foregroundColor);
+            icicles.setRadioPanelPixelColor(panel.index,
+                fromLeft ? i : (panel.colors.length - 1 - i), foregroundColor);
           }
         }
 
@@ -52,7 +59,6 @@ void main() async {
   }
 
   final colors = [
-    Colors.black,
     Colors.orange,
     Colors.oceanBlue,
     Colors.magenta,
@@ -60,7 +66,6 @@ void main() async {
     Colors.lightBlue,
     Colors.violet,
     Colors.black,
-    Colors.black
   ];
 
   bounce(colors);
